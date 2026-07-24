b73600537f5bdfd88e42bf5b0dbd01904b7e9b56
Emre Dayangac <emreday01@gmail.com>
2026-07-24 10:08:10 +0300

    Update test/test_stringart.ml

--- test/test_stringart.ml
+++ test/test_stringart.ml
@@ -2,6 +2,8 @@ let () =
   Alcotest.run "stringart"
     [
       Test_image.suite;
+      Test_oklab.suite;
+      Test_kmeans.suite;
       Test_geometry.suite;
       Test_raster.suite;
       Test_palette.suite;
